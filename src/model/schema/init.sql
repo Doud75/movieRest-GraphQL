1973d25a63b177bec23334966b1a91c06e5ad635
Doud75 <adrienquimbre@gmail.com>
2025-03-13 08:53:13 +0100

    school

--- src/model/schema/init.sql
+++ src/model/schema/init.sql
@@ -1,15 +1,9 @@
 /*
 Script de création de la base de données `school`.
 */
-SET NAMES utf8mb4;
-SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
-SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
-SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
-
-DROP SCHEMA IF EXISTS sakila;
-CREATE SCHEMA sakila;
+create database IF NOT EXISTS school;
 
 /* Créer l'utilisateur API */
 create user IF NOT EXISTS 'api-dev'@'%.%.%.%' identified by 'api-dev-password';
-grant select, update, insert, delete on sakila.* to 'api-dev'@'%.%.%.%';
+grant select, update, insert, delete on school.* to 'api-dev'@'%.%.%.%';
 flush privileges;
